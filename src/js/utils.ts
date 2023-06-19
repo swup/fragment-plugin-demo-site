931199dab6f4ac9eec3bb5bd09333bc2ec13aeb9
Rasso Hilber <mail@rassohilber.com>
2023-06-19 16:38:41 +0200

    Add prev/next post nav

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,6 +1,11 @@
 import { getCollection, CollectionEntry } from "astro:content";
 
 export async function getSortedCharacters(): Promise<CollectionEntry<"characters">[]> {
-  return (await getCollection("characters"))
+  let cache: CollectionEntry<"characters">[] | undefined;
+  if (cache) return cache;
+
+  const posts = (await getCollection("characters"))
     .sort((a, b) => (a.data.sortOrder || 10000) - (b.data.sortOrder || 10000));
+  cache = posts;
+  return posts;
 }
