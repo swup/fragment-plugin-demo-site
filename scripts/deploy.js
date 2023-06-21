49d46d5c1b3fdfacf3ce4b005adc43c7a5b3d135
Rasso Hilber <mail@rassohilber.com>
2023-06-21 09:06:12 +0200

    Add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import { exec, execSync } from "child_process";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+const { RSYNC_PATH } = process.env;
+
+// Validate env variables
+if (!RSYNC_PATH) {
+  throw new Error(`"RSYNC_PATH" is not defined.`);
+}
+
+// Validate current branch
+const branch = execSync("git branch --show", {
+  encoding: "utf8",
+}).trim();
+
+if (branch !== "master") {
+  throw new Error(`Deploying is only possible from the master branch.`);
+}
+
+// Deploy
+execSync(
+  `npm run build && rsync -avz --delete --copy-links ./dist/ ${RSYNC_PATH}`,
+  // Print progress to console
+  // @see https://stackoverflow.com/a/31104898/586823
+  { stdio: "inherit" }
+);
