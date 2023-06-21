e7e7d19dfde3165991f3a83b754af6bc725cf20f
Rasso Hilber <mail@rassohilber.com>
2023-06-21 09:16:00 +0200

    Rename utils.ts to backend.ts

--- src/js/backend.ts
+++ src/js/backend.ts
@@ -2,7 +2,6 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { getCollection, CollectionEntry } from "astro:content";
-import {icons, FeatherIconNames} from 'feather-icons';
 
 /**
  * Get the characters, sorted, cached
