ef9d32a56edc71dcadbddd12289d464282e55665
Rasso Hilber <mail@rassohilber.com>
2023-06-23 17:15:48 +0200

    Use `import.meta.env.DEV` instead of `MODE`

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import { fileURLToPath, URL } from 'node:url';
 
 const resolve = (path) => fileURLToPath(new URL(path, import.meta.url));
 
+console.log('production:', import.meta.env.PROD);
+console.log('development:', import.meta.env.DEV);
+console.log(import.meta.env.MODE);
+
 const __FRAGMENT_PLUGIN_PATH__ =
-  import.meta.env.MODE === "development"
+  import.meta.env.DEV
     ? resolve("./packages/fragment-plugin/src/index.js")
     : "@swup/fragment-plugin";
 
