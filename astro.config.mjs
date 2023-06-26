5a304f6531eafb9e20681e7b7f7b1cfa980f941b
Rasso Hilber <mail@rassohilber.com>
2023-06-26 22:47:45 +0200

    Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { fileURLToPath, URL } from 'node:url';
-
-const __FRAGMENT_PLUGIN_PATH__ =
-  import.meta.env.DEV
-    ? fileURLToPath(new URL("./packages/fragment-plugin/src/index.js", import.meta.url))
-    : "@swup/fragment-plugin";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +10,6 @@ export default defineConfig({
     resolve: {
       alias: {
         "@packages/*": "./packages/*",
-        __FRAGMENT_PLUGIN_PATH__
       },
     },
   },
