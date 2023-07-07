db24bff05c9a880d4ad237ce6dbc57be2338271d
Rasso Hilber <mail@rassohilber.com>
2023-07-07 21:46:32 +0200

    Add `astro-expressive-code` integration
    Used to highlight snippets in code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { fileURLToPath } from "url";
+import expressiveCode from "astro-expressive-code";
+const local = path => fileURLToPath(new URL(path, import.meta.url));
 
-const local = (path) => fileURLToPath(new URL(path, import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [sitemap(), expressiveCode(), mdx()],
   vite: {
     resolve: {
       alias: {
         swup: local("./packages/swup/src/index.js"),
-        "@swup/fragment-plugin": local(
-          "./packages/fragment-plugin/src/index.js"
-        ),
-      },
-    },
+        "@swup/fragment-plugin": local("./packages/fragment-plugin/src/index.js")
+      }
+    }
   },
-  trailingSlash: "always",
+  trailingSlash: "always"
 });
