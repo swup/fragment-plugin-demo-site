c27133b09dd86fcd7d82d5a564f899c514659176
Rasso Hilber <mail@rassohilber.com>
2023-07-08 10:59:22 +0200

    Use astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import { fileURLToPath } from "url";
 import expressiveCode from "astro-expressive-code";
 const local = path => fileURLToPath(new URL(path, import.meta.url));
 
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const expressiveCodeOptions = {
+  // Example: Change the theme to "dracula"
+  // theme: 'dracula',
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [sitemap(), expressiveCode(), mdx()],
+  integrations: [sitemap(), expressiveCode(expressiveCodeOptions), mdx()],
   vite: {
     resolve: {
       alias: {
